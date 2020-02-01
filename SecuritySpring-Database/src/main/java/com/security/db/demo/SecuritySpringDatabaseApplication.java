4aed91a1ef6f42338e7d2e4ece1a344ce23a2f95
sanjeevbellur <sanjeevbellur@10.29.4.35>
2020-02-01 15:05:57 -0500

    [added security Database]

---
+++ SecuritySpring-Database/src/main/java/com/security/db/demo/SecuritySpringDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.security.db.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySpringDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritySpringDatabaseApplication.class, args);
+	}
+
+}
---
+++ SecuritySpring-Database/src/test/java/com/security/db/demo/SecuritySpringDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.db.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySpringDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
