da219de30034e6a724694170e0091c52ff1f7a79
sanjeevbellur <sanjeevbellur@10.29.4.35>
2020-02-02 00:45:09 -0500

    commit -

--- SecuritySpring-Database/src/main/java/com/security/db/demo/SecurityConfiguration.java
+++ SecuritySpring-Database/src/main/java/com/security/db/demo/SecurityConfiguration.java
@@ -9,7 +9,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -22,12 +25,29 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	public AuthenticationProvider ap() {
 		DaoAuthenticationProvider daoap = new DaoAuthenticationProvider();
 		daoap.setUserDetailsService(userDetailsService);
-		daoap.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		daoap.setPasswordEncoder(new BCryptPasswordEncoder());
 		return daoap;
 	}
 	
 	
-	
+	@Override
+	protected void configure(HttpSecurity httpSec)  throws Exception{
+		
+		httpSec.csrf().disable()
+		.authorizeRequests()
+		.antMatchers("/login").permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.formLogin().loginPage("/login").permitAll()
+		.and()
+		.logout()
+		.invalidateHttpSession(true)
+		.clearAuthentication(true)
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		.logoutSuccessUrl("/logout-success").permitAll();
+		
+		
+	}
 	
 
 }
--- SecuritySpring-Database/src/main/resources/schema.sql
+++ SecuritySpring-Database/src/main/resources/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE USER (
 );
 
 
-INSERT INTO USER (id,username,password) VALUES (1,'India','India');
+INSERT INTO USER (id,username,password) VALUES (1,'India','$2y$12$RxIUt2ZJZpkeNbbrN5g5UukeLYEcqbFOIIkQkOKN8kaiL11DuTSSa');
 INSERT INTO USER (id,username,password) VALUES (2,'Indi1','India');
 INSERT INTO USER (id,username,password) VALUES (3,'Indi2','India');
 INSERT INTO USER (id,username,password) VALUES (4,'Indi3','India');
