e937b7e4f0c063db53f89c463450d68073586bc2
sanjeevbellur <sanjeevbellur@10.29.4.237>
2020-02-15 15:44:26 -0500

    [ooath2] - Google implementation

--- SecuritySpring-Database/src/main/java/com/security/db/demo/Controller.java
+++ SecuritySpring-Database/src/main/java/com/security/db/demo/Controller.java
@@ -1,9 +1,13 @@
 package com.security.db.demo;
 
+import java.security.Principal;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@org.springframework.stereotype.Controller
 public class Controller {
 	
 	@GetMapping("/start")
@@ -11,4 +15,19 @@ public class Controller {
 		return "Welcome to Starter";
 	}
 
+	@GetMapping("/login")
+	public String login() {
+		return "login.html";
+	}
+
+	@GetMapping("/logout")
+	public String logout() {
+		return "logout.html";
+	}
+
+	@RequestMapping("user")
+	@ResponseBody
+	public Principal user(Principal principal) {
+		return principal;
+	}
 }
--- SecuritySpring-Database/src/main/java/com/security/db/demo/SecurityConfigurationOoath2.java
+++ SecuritySpring-Database/src/main/java/com/security/db/demo/SecurityConfigurationOoath2.java
@@ -15,42 +15,24 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
-//	
-//	@Autowired
-//	UserDetailsService userDetailsService;
-//	
-//	@Bean
-//	public AuthenticationProvider ap() {
-//		DaoAuthenticationProvider daoap = new DaoAuthenticationProvider();
-//		daoap.setUserDetailsService(userDetailsService);
-//		daoap.setPasswordEncoder(new BCryptPasswordEncoder());
-//		return daoap;
-//	}
-//	
-//	
-//	@Override
-//	protected void configure(HttpSecurity httpSec)  throws Exception{
-//		
-//		httpSec.csrf().disable()
-//		.authorizeRequests()
-//		.antMatchers("/login").permitAll()
-//		.antMatchers("/h2-console").permitAll()
-//		.antMatchers("/h2-console/*").permitAll()
-//		.anyRequest().authenticated()
-//		.and()
-//		.formLogin().loginPage("/login").permitAll()
-//		.and()
-//		.logout()
-//		.invalidateHttpSession(true)
-//		.clearAuthentication(true)
-//		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-//		.logoutSuccessUrl("/logout-success").permitAll();
-//		
-//		
-//	}
-//	
-//
-//}
+@Configuration
+@EnableWebSecurity
+@EnableOAuth2Sso
+public class SecurityConfigurationOoath2 extends WebSecurityConfigurerAdapter{
+	
+	
+	@Override
+	protected void configure(HttpSecurity httpSec)  throws Exception{
+		
+		httpSec.csrf().disable()
+		.authorizeRequests()
+		.antMatchers("/login").permitAll()
+		.antMatchers("/h2-console").permitAll()
+		.antMatchers("/h2-console/*").permitAll()
+		.anyRequest().authenticated();
+		
+		
+	}
+	
+
+}
